cdbcee98dbb73cf5831ca8a44d514a588436100f
Lorain <743804605@qq.com>
2023-08-08 17:07:29 +0800

    add otel provider

--- middleware/requestid/traceid/main.go
+++ middleware/requestid/traceid/main.go
@@ -6,12 +6,18 @@ import (
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/hertz-contrib/obs-opentelemetry/provider"
 	"github.com/hertz-contrib/obs-opentelemetry/tracing"
 	"github.com/hertz-contrib/requestid"
 	"go.opentelemetry.io/otel/trace"
 )
 
 func main() {
+	provider.NewOpenTelemetryProvider(
+		provider.WithServiceName("trace"),
+		provider.WithExportEndpoint(":4317"),
+		provider.WithInsecure(),
+	)
 	tracer, cfg := tracing.NewServerTracer()
 	h := server.Default(tracer)
 	h.Use(requestid.New(requestid.WithGenerator(func(ctx context.Context, c *app.RequestContext) string {
