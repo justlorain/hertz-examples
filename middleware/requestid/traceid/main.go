347778f5afc0e1dd9fdf3187e7987dae7eb9bbb5
Lorain <743804605@qq.com>
2023-08-08 16:56:21 +0800

    feat: add traceid example

---
+++ middleware/requestid/traceid/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/hertz-contrib/obs-opentelemetry/tracing"
+	"github.com/hertz-contrib/requestid"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func main() {
+	tracer, cfg := tracing.NewServerTracer()
+	h := server.Default(tracer)
+	h.Use(requestid.New(requestid.WithGenerator(func(ctx context.Context, c *app.RequestContext) string {
+		traceID := trace.SpanFromContext(ctx).SpanContext().TraceID().String()
+		return traceID
+	})), tracing.ServerMiddleware(cfg))
+	h.GET("/get", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "success")
+	})
+	h.POST("/post", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "success")
+	})
+	h.Spin()
+}
